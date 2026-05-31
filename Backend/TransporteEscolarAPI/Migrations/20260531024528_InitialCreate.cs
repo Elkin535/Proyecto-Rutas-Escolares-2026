48de338d19809fe8772b0ec24b64b6315f51888c
Elkin535 <145403903+Elkin535@users.noreply.github.com>
2026-05-30 23:34:32 -0500

    Agregar migración inicial

---
+++ Backend/TransporteEscolarAPI/Migrations/20260531024528_InitialCreate.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TransporteEscolarAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "acudiente",
+                columns: table => new
+                {
+                    id_acudiente = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_usuario = table.Column<int>(type: "integer", nullable: false),
+                    direccion_residencia = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_acudiente", x => x.id_acudiente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "conductor",
+                columns: table => new
+                {
+                    id_conductor = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_usuario = table.Column<int>(type: "integer", nullable: false),
+                    id_vehiculo = table.Column<int>(type: "integer", nullable: true),
+                    numero_licencia = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    categoria_licencia = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_conductor", x => x.id_conductor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "estudiante",
+                columns: table => new
+                {
+                    id_estudiante = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_acudiente = table.Column<int>(type: "integer", nullable: false),
+                    nombre = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    apellido = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    colegio = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    curso_grado = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    estado = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_estudiante", x => x.id_estudiante);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "historial",
+                columns: table => new
+                {
+                    id_viaje = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_vehiculo = table.Column<int>(type: "integer", nullable: false),
+                    id_conductor = table.Column<int>(type: "integer", nullable: false),
+                    fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    hora_inicio = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    hora_fin = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    estado_viaje = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    latitud_actual = table.Column<decimal>(type: "numeric", nullable: true),
+                    longitud_actual = table.Column<decimal>(type: "numeric", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_historial", x => x.id_viaje);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rol",
+                columns: table => new
+                {
+                    id_rol = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombre_rol = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rol", x => x.id_rol);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    id_usuario = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_rol = table.Column<int>(type: "integer", nullable: false),
+                    nombre = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    apellido = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    correo = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    contrasena = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    telefono = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    fecha_creacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuario", x => x.id_usuario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehiculo",
+                columns: table => new
+                {
+                    id_vehiculo = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    placa = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    modelo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    capacidad_pasajeros = table.Column<int>(type: "integer", nullable: false),
+                    soat_vencimiento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    tecnomecanica_vencimiento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehiculo", x => x.id_vehiculo);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "acudiente");
+
+            migrationBuilder.DropTable(
+                name: "conductor");
+
+            migrationBuilder.DropTable(
+                name: "estudiante");
+
+            migrationBuilder.DropTable(
+                name: "historial");
+
+            migrationBuilder.DropTable(
+                name: "rol");
+
+            migrationBuilder.DropTable(
+                name: "usuario");
+
+            migrationBuilder.DropTable(
+                name: "vehiculo");
+        }
+    }
+}
